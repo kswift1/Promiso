3ec3647e56bacb95146a162b4b60ca06ec4d2a52
sungwon-swing <kswen0203@gmail.com>
2025-09-04 17:58:57 +0900

    chore: remove Xcode generated projects/workspaces (Tuist-managed)

--- Projects/App/Sources/Assembly.swift
+++ Projects/App/Sources/Assembly.swift
@@ -5,9 +5,4 @@
 //  Created by 김성원 on 9/4/25.
 //
 
-import MainFeatureInterface
-import MainFeatureImplement
-
-enum Factories {
-  static func main() -> MainEntry { .live() }
-}
+import Foundation
