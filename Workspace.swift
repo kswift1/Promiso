b9aa3f16d156f1a431ff60f5f9477e4cd0bf38ba
sungwon-swing <kswen0203@gmail.com>
2025-09-08 22:12:17 +0900

    feat: AppFeatureDeps 중복 제거

--- Workspace.swift
+++ Workspace.swift
@@ -1,8 +1,8 @@
-// Workspace.swift
 import ProjectDescription
+import ProjectDescriptionHelpers
 
 let workspace = Workspace(
-  name: "StopLate",
+  name: AppConfig.name,
   projects: [
     "Projects/App",   // 앱
     "Projects/**"     // Features/Clients/Shared/Extensions 전체
