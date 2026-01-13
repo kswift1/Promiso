e49ea3c72c0ffc9f9cf82a0cd2b726fd2c397ce7
sungwon-swing <kswen0203@gmail.com>
2026-01-13 15:33:20 +0900

    fix: logLevel verbose -> debug

--- Projects/App/Sources/AppDelegate.swift
+++ Projects/App/Sources/AppDelegate.swift
@@ -29,7 +29,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     let projectId = "v0o95eccpc"
 
     #if DEBUG
-    let config = ClarityConfig(projectId: projectId, logLevel: .verbose)
+    let config = ClarityConfig(projectId: projectId, logLevel: .debug)
     #else
     let config = ClarityConfig(projectId: projectId, logLevel: .none)
     #endif
